e9c0ffbb30ceed62099789750eb2a48fd41eaa2b
Stan Le <stan.le1107@gmail.com>
2015-08-08 20:33:09 -0400

    launch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var proxy = httpProxy.createProxyServer();
 var app = express();
 
 var isProduction = process.env.NODE_ENV === 'production';
+console.log(isProduction)
 var port = isProduction ? 8080 : process.env.PORT;
 var publicPath = path.resolve(__dirname, 'public');
 
