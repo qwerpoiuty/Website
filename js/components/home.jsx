92a9f9f32a36e304c3b0305dcca8cb739cfc9f58
Stan Le <stan.le1107@gmail.com>
2015-10-13 19:07:50 -0700

    more stuff

--- js/components/home.jsx
+++ js/components/home.jsx
@@ -36,9 +36,11 @@ let Home = React.createClass({
 		    			<div className = "projectContainer">
 		    				<ul>
 		    					<li><a href = "http://www.quokka.chat" target = "_blank" ><h2 className = "projectName">QuokkaChat</h2></a>
-			    				<h3 className = "projectDescription">A webapp to remove some of the noise involved with commenting on videos. Organizes comments based on the video time they were made.</h3></li>
+			    				<h3 className = "projectDescription">A webapp to remove some of the noise involved with commenting on videos. Organizes comments based on the video time they were made. Comments on screen will update as the video plays!</h3></li>
 		    					<li><a href = "https://github.com/qwerpoiuty/FaceOff" target = "_blank" ><h2 className = "projectName">FaceOff</h2></a>
-		    					<h3 className = "projectDescription">A game 2 person game where you use your face position to control your block and try to shoot down your opponent with simple math</h3></li>
+		    					<h3 className = "projectDescription">A game 2 person game where you literally have to dodge incoming fire. Fire back at your opponent with simple math questions.</h3></li>
+		    					<li><a href = "https://github.com/qwerpoiuty/gameoflife" target = "_blank" ><h2 className = "projectName">Game of Life</h2></a>
+		    					<h3 className = "projectDescription">My version of the Game of Life. Written in vanilla JavaScript.</h3></li>
 		    				</ul>
 		    			</div>
 		    		</div>
